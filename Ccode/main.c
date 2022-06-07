2c22aa26748b737b6b95acb7a076b310b2f7b699
SanGe <zhaojie8173@163.com>
2022-06-07 20:11:00 +0800

    1

--- Ccode/main.c
+++ Ccode/main.c
@@ -2,13 +2,10 @@
 #include <string.h>
 #include "logfiletest.h"
 
-extern int LOG_Init(char *fname);
-
-
-
 int main(void)
 {
+    char *fp = NULL;
     int i = 0;
-    i = LOG_Init(NULL);
+    i = LOG_Init(fp);
     return i;
 }
