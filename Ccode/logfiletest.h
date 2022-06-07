f82c4666f66d15a387ec194204503c1dac05eba8
SanGe <zhaojie8173@163.com>
2022-06-07 20:50:31 +0800

    1

--- Ccode/logfiletest.h
+++ Ccode/logfiletest.h
@@ -1,6 +1,6 @@
 #ifndef LOGFILETEST_H_
 #define LOGFILETEST_H_
 
-extern int LOG_Init(char *);
+int LOG_Init(char *fname);
 
 #endif
--- Ccode/tempCodeRunnerFile.c
+++ Ccode/tempCodeRunnerFile.c
@@ -1,4 +1,1 @@
-    // char *fp = NULL;
-    // int i = 0;
-    // i = LOG_Init(fp);
-    // return i;
+logfiletest
